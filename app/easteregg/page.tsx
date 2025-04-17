d2bce7e69cae1dab6d8db3873e7c543bd0a9c6f1
Edward Zota <ezo@novatec-gmbh.de>
2025-04-17 09:15:55 +0200

    #1: project started, axios installed and home page beginning

--- app/easteregg/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EasterEggPage() {
-    return (
-        <div className="text-center mt-20">
-            <h1 className="text-4xl font-bold">🎉 Congratulation!</h1>
-            <p className="mt-4 text-lg">Have you find the easter egg! 🥚✨</p>
-        </div>
-    );
-}
--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="text-center mt-20">
-            <h1 className="text-3xl font-bold">Page Not Found</h1>
-            <p className="text-gray-600 mt-2">404</p>
-        </div>
-    );
-}
