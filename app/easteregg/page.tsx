935c5799d45648b2e50fcb44ef74bedbc8f6e0f7
Edward Zota <ezo@novatec-gmbh.de>
2025-04-17 08:53:32 +0200

    #1: project started, axios installed and home page beginning

---
+++ app/easteregg/page.tsx
@@ -0,0 +1,8 @@
+export default function EasterEggPage() {
+    return (
+        <div className="text-center mt-20">
+            <h1 className="text-4xl font-bold">🎉 Congratulation!</h1>
+            <p className="mt-4 text-lg">Have you find the easter egg! 🥚✨</p>
+        </div>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      {/* header must be added */}
+      <main>{children}</main>
+      {/* footer must be added */}
       </body>
     </html>
   );
